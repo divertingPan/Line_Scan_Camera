67c942144fcffc19c987350ab0d3b0dfd5f23a55
Diverting Pan <39986072+divertingPan@users.noreply.github.com>
2021-09-02 20:35:07 +0800

    Update bmp2png.py

--- bmp2png.py
+++ bmp2png.py
@@ -1,8 +1,22 @@
+# --- v0.1 ---
+# to transform generated bmp file into png
+# for a better post-processing in Photoshop
+
+# --- v0.2 ---
+# transform images from a whole folder
+# the folder should only comtain .bmp files
+
+import os
 import cv2
 
 
-# to transform generated bmp file into png
-# for a better post-processing in Photoshop
-image_path = 'Y0023.BMP'
-image = cv2.imread(image_path)
-cv2.imwrite('{}_modified.png'.format(image_path[:-4]), image)
+folder = r'LCAM3'
+images = os.listdir(folder)
+if not os.path.exists('{}_modified'.format(folder)):
+    os.makedirs('{}_modified'.format(folder))
+
+for image_path in images:
+    # image_path = 'Y0023.BMP'
+    image = cv2.imread('{}/{}'.format(folder, image_path))
+    cv2.imwrite('{}_modified/{}_modified.png'.format(folder, image_path[:-4]), image)
+    print('{} saved!'.format(image_path))
